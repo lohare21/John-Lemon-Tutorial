fe70e7a1ba784d98530873a642187e00970a6078
lohare21 <lohare@iu.edu>
2022-04-06 11:53:52 -0400

    Having some difficulties, will come back tomorrow to figure out why my code isn't working

--- John Lemon Tutorial/Assets/Scripts/PlayerMove.cs
+++ John Lemon Tutorial/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float playerSpeed = 20f;
+    private CharacterController myCC;
+
+    private Vector3 inputVector;
+    private Vector3 movementVector;
+    private float myGravity = -10;
+
     void Start()
     {
-        
+        myCC = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+        MovePlayer();
+    }
+
+    void GetInput()
+    {
+        inputVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); 
+        inputVector.Normalize();
+        inputVector = transform.TransformDirection(inputVector);
+
+        movementVector = (inputVector * playerSpeed) + (Vector3.up * myGravity);
+    }
+
+    void MovePlayer()
+    {
+        myCC.Move(movementVector * Time.deltaTime);
+
     }
 }
