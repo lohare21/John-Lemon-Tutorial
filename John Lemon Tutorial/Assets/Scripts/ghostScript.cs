418d2f9275330cb81e0997d1fc752f89125a8465
lohare21 <lohare@iu.edu>
2022-04-25 00:11:03 -0400

    Code is correct. ghosts are not moving and just disappearing when I get close

--- John Lemon Tutorial/Assets/Scripts/ghostScript.cs
+++ John Lemon Tutorial/Assets/Scripts/ghostScript.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
-public class ghostScript : MonoBehaviour
+public class ghostScript : MonoBehaviour {
+
+private NavMeshAgent Mob;
+public GameObject Player;
+
+public float MobDistanceRun = 4.0f;
+
+void Start()
 {
-    // Start is called before the first frame update
-    void Start()
+    Mob = GetComponent<NavMeshAgent>();
+}
+void Update()
+{
+    float distance = Vector3.Distance(transform.position, Player.transform.position);
+    if (distance < MobDistanceRun)
     {
-        
+        Vector3 dirToPlayer = transform.position - Player.transform.position;
+        Vector3 newPos = transform.position = dirToPlayer;
+        Mob.SetDestination(newPos);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
 }
